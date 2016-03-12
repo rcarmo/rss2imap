a8be0e120c68bed5d2196670f42d8884e0c43828
unknown <ndelsaux@LFR005669.corp.capgemini.com>
2016-03-12 15:27:47 +0100

    I forgot to rewrite the opmlexport function to support the folders

--- rss2email.py
+++ rss2email.py
@@ -863,10 +863,24 @@ def opmlexport():
     
     if feeds:
         print '<?xml version="1.0" encoding="UTF-8"?>\n<opml version="1.0">\n<head>\n<title>rss2email OPML export</title>\n</head>\n<body>'
-        for f in feeds[1:]:
-            url = xml.sax.saxutils.escape(f.url)
-            print '<outline type="rss" text="%s" xmlUrl="%s"/>' % (url, url)
-        print '</body>\n</opml>'
+		exportableFeeds = {:}
+		if USE_OPML_TITLE_AS_FOLDER:
+			for f in feeds[1:]:
+				if not hasattr(exportableFeeds, f.folder):
+					exportableFeeds[f.folder] = {}
+				exportableFeeds[f.folder].append(f)
+
+			for folder in exportableFeeds:
+				print '\n\t<outline text="%s" title="%s">' % (folder, folder)
+				for f in exportableFeeds[folder]:
+					url = xml.sax.saxutils.escape(f.url)
+					print '\n\t\t<outline type="rss" text="%s" xmlUrl="%s"/>' % (url, url)
+				print '\n\t</outline>'
+		else:
+	        for f in feeds[1:]:
+				url = xml.sax.saxutils.escape(f.url)
+				print '<outline type="rss" text="%s" xmlUrl="%s"/>' % (url, url)
+        print '\n</body>\n</opml>'
 
 def opmlimport(importfile):
     importfileObject = None
